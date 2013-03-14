7b3b7d337b8e632ae78ad53441945cee76383c2d
SRenan <renan.sauteraud@gmail.com>
2013-03-14 02:33:34 -0700

    Removing .dat

--- script.R
+++ script.R
@@ -4,7 +4,7 @@ sl<-slummarize(bl)
 msl<-melt(sl)
 msl.ss<-subset(msl,tolower(sample_type)=="standard")
 ggplot2::ggplot(msl.ss, ggplot2::aes(color=plate), alpha=0.5)+ggplot2::scale_x_log10()+ggplot2::scale_y_log10()+ggplot2::facet_wrap(~analyte)+geom_sc(sl)    +ggplot2::geom_point(ggplot2::aes(x=concentration, y=mfi))
-plot_layout(sl, plate_name="plate2", fill="sample_type")
+plot_layout(sl, plate_name="plate1", fill="sample_type")
 
 #BioPlex with specified analyte and layout
 bl<-read.experiment("YALE")
@@ -12,7 +12,7 @@ sl<-slummarize(bl)
 msl<-melt(sl)
 msl.ss<-subset(msl,tolower(sample_type)=="standard")
 ggplot2::ggplot(msl.ss, ggplot2::aes(color=plate), alpha=0.5)+ggplot2::scale_x_log10()+ggplot2::scale_y_log10()+ggplot2::facet_wrap(~analyte)+geom_sc(sl)    +ggplot2::geom_point(ggplot2::aes(x=concentration, y=mfi))
-plot_layout(sl, plate_name="plate2", fill="sample_type")
+plot_layout(sl, plate_name="plate1", fill="sample_type")
 
 #xPonent 3.x without mapping
 setup_templates("LENA")
