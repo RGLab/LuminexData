aa4069b51fb7fe7925ccf95b935432e63f339b46
SRenan <renan.sauteraud@gmail.com>
2013-03-14 01:50:17 -0700

    Updated test script

---
+++ script.R
@@ -0,0 +1,19 @@
+#xPonent 1.x Multiplate with specified analyte and layout
+bl<-read.experiment("csvtest")
+sl<-slummarize(bl)
+msl<-melt(sl)
+msl.ss<-subset(msl,tolower(sample_type)=="standard")
+ggplot2::ggplot(msl.ss, ggplot2::aes(color=plate), alpha=0.5)+ggplot2::scale_x_log10()+ggplot2::scale_y_log10()+ggplot2::facet_wrap(~analyte)+geom_sc(sl)    +ggplot2::geom_point(ggplot2::aes(x=concentration, y=mfi))
+plot_layout(sl, plate_name="plate2", fill="sample_type")
+
+#BioPlex with specified analyte and layout
+bl<-read.experiment("YALE")
+sl<-slummarize(bl)
+msl<-melt(sl)
+msl.ss<-subset(msl,tolower(sample_type)=="standard")
+ggplot2::ggplot(msl.ss, ggplot2::aes(color=plate), alpha=0.5)+ggplot2::scale_x_log10()+ggplot2::scale_y_log10()+ggplot2::facet_wrap(~analyte)+geom_sc(sl)    +ggplot2::geom_point(ggplot2::aes(x=concentration, y=mfi))
+plot_layout(sl, plate_name="plate2", fill="sample_type")
+
+#xPonent 3.x without mapping
+setup_templates("LENA")
+bl<-read.experiment("LENA")
